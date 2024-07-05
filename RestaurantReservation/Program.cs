14d238805a12e05d1f666ae5abbc42c65465fe77
Bayan Aboalrob <bayanaboalrob2001@gmail.com>
2024-07-05 12:34:59 +0300

    Add project files.

---
+++ RestaurantReservation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantReservation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Restaurant Reservation Application !");
+        }
+    }
+}
